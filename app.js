9e1ebbf2d2eabca2eca8597b7fec70cca4cd0568
Vlad Minajev <vladminajev@MacBook-Pro-Vlad.local>
2017-02-24 16:27:27 +0200

    Esimene ülesanne tehtud
    Lisatud leht, kus on mangu kirjeldus, nimi välju, start nupp. Probleem:
    kui panen <style> - style.css faili ja annan sellele lingi, siis
    vajutades «start» ei kao ära home-view, aga game-view renderdakse. Kui
    <style> on html sees, siis töötab.

---
+++ app.js
@@ -0,0 +1,101 @@
+(function() {
+    "use strict";
+
+    var game = function() {
+
+        // SEE ON SINGLETON PATTERN
+        if (game.instance) {
+            return game.instance;
+        }
+        //this viitab game fn
+        game.instance = this;
+
+        this.routes = game.routes;
+        // saame lehe laadimiseks kasutada hiljem this.routes['home-view'].render()
+
+        console.log('moosipurgi sees');
+
+        // KÕIK muuutujad, mida muudetakse ja on rakendusega seotud defineeritakse siin
+        this.click_count = 0;
+        this.currentRoute = null;
+
+        //id, mis läheb purgile kaasa, alustame nullist, tegelikult võiks olla unikaalne
+        this.jar_id = 0;
+
+        // hakkan hoidma siin kõiki purke
+        this.jars = [];
+
+        // Kui tahan Moosipurgile referenci siis kasutan THIS = MOOSIPURGI RAKENDUS ISE
+        this.init();
+    };
+
+    // kõik lehed, teistes raamistikes võib käsitleda neid view'dena
+    // render funktsioon käivtatakse selle konkreetse lehe laadimisel
+    game.routes = {
+        'home-view': {
+            'render': function() {
+                // käivitame siis kui lehte laeme
+                console.log('>>>>avaleht');
+            }
+        },
+        'game-view': {
+            'render': function() {
+                // käivitame siis kui lehte laeme
+                console.log('>>>>loend');
+
+                var typerGame = new TYPER();
+                window.typerGame = typerGame;
+              }
+        },
+    };
+
+    // Kõik funktsioonid lähevad Moosipurgi külge
+    game.prototype = {
+
+        init: function() {
+            console.log('Rakendus läks tööle');
+
+            //kuulan aadressirea vahetust, ehk kui aadressireale tuleb #lehe nimi
+            window.addEventListener('hashchange', this.routeChange.bind(this));
+
+            // kui aadressireal ei ole hashi siis lisan juurde, et avaleht
+            if (!window.location.hash) {
+                window.location.hash = 'home-view';
+                // routechange siin ei ole vaja sest käsitsi muutmine käivitab routechange event'i ikka
+            } else {
+                //esimesel käivitamisel vaatame urli üle ja uuendame menüüd
+                this.routeChange();
+            }
+        },
+
+
+        routeChange: function(event) {
+
+            //kirjutan muuutujasse lehe nime, võtan maha #
+            this.currentRoute = location.hash.slice(1);
+            console.log(this.currentRoute);
+
+            //kas meil on selline leht olemas?
+            if (this.routes[this.currentRoute]) {
+
+                //muudan menüü lingi aktiivseks
+
+                this.routes[this.currentRoute].render();
+
+
+            } else {
+                /// 404 - ei olnud
+            }
+
+
+        },
+
+    }; // MOOSIPURGI LÕPP
+
+
+    // kui leht laetud käivitan Moosipurgi rakenduse
+    window.onload = function() {
+        var app = new game();
+    };
+
+})();
