03972b872c3f2628c084bbe9b80f996be234ca82
Hinrek Saar <hinrek@gmail.com>
2017-03-06 11:34:08 +0200

    Changed font and font size to %

--- Word.js
+++ Word.js
@@ -19,7 +19,7 @@ Word.prototype = {
 
 		// Canvasele joonistamine
 		this.ctx.textAlign = 'center';
-		this.ctx.font = '70px Courier';
+		this.ctx.font = '250% PT Serif, PT Sans, serif, sans-serif';
 
 		// 	// Joonistame sõna, mis on järel / tekst, x, y
 		this.ctx.fillText(this.left, this.canvas.width/2, this.canvas.height/2);
