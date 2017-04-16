e3d9254725842750a3f1a36f169390f193401fc6
alaraasa <alar@alaraasa.ee>
2017-04-16 13:06:35 +0300

    Timer working.

--- TYPER.js
+++ TYPER.js
@@ -18,7 +18,16 @@ var TYPER = function () {
     this.guessed_words = 0; // arvatud sõnade arv
 
     this.timer = 0;
+    var that = this;
+    this.counter = setInterval(function(){
+        that.timer++;
+        this.timer = that.timer;
 
+    }, 1000);
+
+
+    this.currentWordTimer = 0;
+    console.log(this.currentWordTimer);
     //mängija objekt, hoiame nime ja skoori
     this.player = {name: null, score: 0, highScore: 0};
 
@@ -51,9 +60,9 @@ TYPER.prototype = {
     loadPlayerData: function () {
 
         // küsime mängija nime ja muudame objektis nime
-        var p_name;
+        let p_name;
 
-        if (this.player.name === null){
+        if (this.player.name === null) {
             p_name = prompt("Please enter your name. Leave blank for Anonymous");
             // Kui ei kirjutanud nime või jättis tühjaks
             if (p_name === null || p_name === "") {
@@ -65,18 +74,32 @@ TYPER.prototype = {
 
         //new session
         if (localStorage.getItem("highScores") === null) {
-            var newLocalStorage = {};
-            localStorage.setItem("highScores", JSON.stringify(newLocalStorage));
-            newPlayer(p_name);
-            this.data = localStorage.getItem("highScores");
+            console.log("NEW SESSION");
+            let newLocalStorage = [{}];
+            this.saveToLocalStorage(newLocalStorage);
+            this.newPlayer(this.player.name);
         } else {
-            if(this.data[p_name] === undefined){
-                newPlayer(p_name);
+            this.data = JSON.parse(localStorage.getItem("highScores"));
+            console.log("OLD SESSION");
+            console.log(this.data);
+
+            let found = false;
+            for (let user in this.data) {
+                if (this.data.hasOwnProperty(user)) {
+                    if (user.name === this.player.name) {
+                        console.log("OLD PLAYER");
+
+                        found = true;
+                    }
+                }
+
+                if (!found) this.newPlayer(this.player.name);
+            }
         }
 
         // Mänigja objektis muudame nime
         this.player.name = p_name; // player =>>> {name:"Romil", score: 0, highScore:0}
-        console.log(this.player);
+        //console.log(this.player);
     },
 
     loadWords: function () {
@@ -84,7 +107,7 @@ TYPER.prototype = {
         console.log('loading...');
 
         // AJAX http://www.w3schools.com/ajax/tryit.asp?filename=tryajax_first
-        var xmlhttp = new XMLHttpRequest();
+        let xmlhttp = new XMLHttpRequest();
 
         // määran mis juhtub, kui saab vastuse
         xmlhttp.onreadystatechange = function () {
@@ -92,24 +115,32 @@ TYPER.prototype = {
             //console.log(xmlhttp.readyState); //võib teoorias kõiki staatuseid eraldi käsitleda
 
             // Sai faili tervenisti kätte
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 
                 console.log('successfully loaded');
 
                 // serveri vastuse sisu
-                var response = xmlhttp.responseText;
+                let response = xmlhttp.responseText;
                 //console.log(response);
 
                 // tekitame massiivi, faili sisu aluseks, uue sõna algust märgib reavahetuse \n
-                var words_from_file = response.split('\n');
+                let words_from_file = response.split('\n');
                 //console.log(words_from_file);
 
                 // Kuna this viitab siin xmlhttp päringule siis tuleb läheneda läbi avaliku muutuja
                 // ehk this.words asemel tuleb kasutada typerGame.words
 
+
+                this.data = JSON.parse(localStorage.getItem("highScores"));
+                console.log("ONLOAD");
+                console.log(this.data);
+                console.log(localStorage.getItem("highScores"));
+                console.log("/ONLOAD");
+
+
                 //asendan massiivi
                 typerGame.words = structureArrayByWordLength(words_from_file);
-                console.log(typerGame.words);
+                //console.log(typerGame.words);
 
                 // küsime mängija andmed
                 typerGame.loadPlayerData();
@@ -142,17 +173,21 @@ TYPER.prototype = {
 
         // kui pikk peab sõna tulema, + min pikkus + äraarvatud sõnade arvul jääk 5 jagamisel
         // iga viie sõna tagant suureneb sõna pikkus ühe võrra
-        var generated_word_length = this.word_min_length + parseInt(this.guessed_words / 5);
+        let generated_word_length = this.word_min_length + parseInt(this.guessed_words / 5);
 
         // Saan suvalise arvu vahemikus 0 - (massiivi pikkus -1)
-        var random_index = (Math.random() * (this.words[generated_word_length].length - 1)).toFixed();
+        let random_index = (Math.random() * (this.words[generated_word_length].length - 1)).toFixed();
 
         // random sõna, mille salvestame siia algseks
-        var word = this.words[generated_word_length][random_index];
+        let word = this.words[generated_word_length][random_index];
 
-        this.timer = 0;
         // Word on defineeritud eraldi Word.js failis
         this.word = new Word(word, this.canvas, this.ctx);
+        //start timer
+        this.currentWordTimer = this.timer;
+
+        console.log("189 - Current word timer:" + this.currentWordTimer);
+
 
     },
 
@@ -160,7 +195,7 @@ TYPER.prototype = {
 
         //console.log(event);
         // event.which annab koodi ja fromcharcode tagastab tähe
-        var letter = String.fromCharCode(event.which);
+        let letter = String.fromCharCode(event.which);
         //console.log(letter);
 
         // Võrdlen kas meie kirjutatud täht on sama mis järele jäänud sõna esimene
@@ -176,7 +211,13 @@ TYPER.prototype = {
                 this.guessed_words += 1;
 
                 //update player score
-                this.player.score += /*this.timer * */Math.ceil(this.guessed_words / 5);
+                let timeMultiplier;
+                if((this.currentWordTimer.valueOf()+10)-this.timer.valueOf() < 0) {
+                    timeMultiplier = 1;
+                } else {
+                    timeMultiplier = (this.currentWordTimer.valueOf()+10)-this.timer.valueOf();
+                }
+                this.player.score += timeMultiplier * Math.ceil(this.guessed_words / 5);
 
                 //loosin uue sõna
                 this.generateWord();
@@ -185,23 +226,81 @@ TYPER.prototype = {
             //joonistan uuesti
             this.word.Draw();
         } else {
-            if(this.player.score > this.player.highScore){
+            if (this.player.score > this.player.highScore) {
                 this.player.highScore = this.player.score;
             }
-            while(this.word.left.length !== 1){
+            while (this.word.left.length !== 1) {
                 this.word.removeFirstLetter();
-                this.word.left.length = this.word.left.length;
+                //this.word.left.length = this.word.left.length;
             }
             alert("Game over! \nYour score: " + this.player.score + "\nYour high score: " + this.player.highScore);
             this.guessed_words = 0;
             this.player.score = 0;
-            this.timer = 0;
-            gameOver();
+            this.gameOver();
             this.generateWord();
             this.word.Draw();
 
         }
 
+    },
+
+    gameOver: function () {
+        //Move to high score table
+        document.getElementById('scores').scrollIntoView();
+        //Update player data, if necessary
+        let playerName = this.player.name;
+        for (let user in this.data) {
+            if (this.data.hasOwnProperty(user)) {
+                if (user.name === playerName) {
+                    if (user.score < this.player.score) {
+                        this.updateScore(playerName, this.player.score);
+                    }
+                }
+            }
+        }
+        console.log(this.data);
+        //Update table
+        generateTable(this.data);
+    },
+
+    updateScore: function (playerName, newScore) {
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i].name === playerName) {
+                this.data[i].score = newScore;
+                this.saveToLocalStorage(this.data);
+            }
+        }
+    },
+    newPlayer: function (playerName) {
+        console.log("NEW PLAYER");
+
+        let newPlayer = {
+            name: playerName,
+            score: 0,
+            wordsGuessed: 0
+        };
+
+        this.data.push(newPlayer);
+        console.log(this.data.valueOf());
+        this.saveToLocalStorage(this.data);
+    },
+
+
+    saveToLocalStorage: function (json) {
+        localStorage.setItem("highScores", JSON.stringify(json));
+        this.data = JSON.parse(localStorage.getItem("highScores"));
+        console.log(localStorage.getItem("highScores"));
+
+    },
+
+
+    resetTimer: function () {
+        console.log("Word timer reset");
+        this.currentWordTimer = this.timer;
+    },
+
+    timerCount: function() {
+        this.timer++;
     }
 
 };
@@ -213,12 +312,12 @@ function structureArrayByWordLength(words) {
     // NT this.words[3] on kõik kolmetähelised
 
     // defineerin ajutise massiivi, kus kõik on õiges jrk
-    var temp_array = [];
+    let temp_array = [];
 
     // Käime läbi kõik sõnad
-    for (var i = 0; i < words.length; i++) {
+    for (let i = 0; i < words.length; i++) {
 
-        var word_length = words[i].length;
+        let word_length = words[i].length;
 
         // Kui pole veel seda array'd olemas, tegu esimese just selle pikkusega sõnaga
         if (temp_array[word_length] === undefined) {
@@ -233,18 +332,10 @@ function structureArrayByWordLength(words) {
     return temp_array;
 }
 
-void function newPlayer(playerName){
-    newPlayer = {
-        [playerName]: [
-                {"score": 0},
-                {"wordsGuessed": 0}
-            ]
-        }
-    }
-    this.data.add(newPlayer);
+function resetTimer() {
+    typerGame.resetTimer();
 }
-
-var requestAnimFrame = (function () {
+const requestAnimFrame = (function () {
     return window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame ||
@@ -254,7 +345,7 @@ var requestAnimFrame = (function () {
 })();
 
 function nightModeToggle() {
-    var currentColor = document.getElementsByTagName("canvas").style["background-color"];
+    let currentColor = document.getElementsByTagName("canvas").style["background-color"];
     if (currentColor === "#e8eaf6") {
         document.getElementById("nightModeToggle").textContent = "Day mode";
         document.getElementById("nightmodeToggle").style.color = "#e8eaf6";
@@ -266,41 +357,40 @@ function nightModeToggle() {
     }
 }
 
-function gameOver() {
-    //Move to high score table
-    document.getElementById('scores').scrollIntoView();
-    //Update player data, if necessary
-    var playerName = this.player.name;
-    if(this.data.users.playerName)
-    //Update table
-    generateTable();
-}
 
-function generateTable(){
+function generateTable(data) {
     //clear table
-    document.getElementById("table").getElementsByTagName('tbody')[0].parentNode
-        .replaceChild(document.createElement('tbody'), table);
-    var table = document.getElementById("table").getElementsByTagName('tbody')[0];
-
-    for (var user in this.data){
-        table.addRow
+    let table = document.getElementById("hiscoreTable").getElementsByTagName('tbody')[0];
+    table.parentNode.replaceChild(document.createElement('tbody'), table);
+    sortData();
+    let i = 0;
+    for (let user in data) {
+        if (this.data.hasOwnProperty(user)) {
+            let row = table.insertRow(i);
+            let nameRow = row.insertCell(0);
+            let scoreRow = row.insertCell(1);
+            let timeRow = row.insertCell(2);
+            nameRow.innerHTML = user.name;
+            scoreRow.innerHTML = user.score;
+            timeRow.innerHTML = user.wordsGuessed;
+        }
+        i++;
     }
 
 }
 
 function sortData() {
-    
+    this.data.sort(function (a, b) {
+        return a.score > b.score;
+    })
 }
 
-function resetTimer(){
-    this.timer = 0;
-}
 
 window.onload = function () {
     var typerGame = new TYPER();
     window.typerGame = typerGame;
-    var nmToggle = document.getElementById("nightModeToggle");
+    const nmToggle = document.getElementById("nightModeToggle");
     nmToggle.onclick = nightModeToggle();
-    this.data = JSON.parse(localStorage.getItem("highScores"));
+
     generateTable();
 };
