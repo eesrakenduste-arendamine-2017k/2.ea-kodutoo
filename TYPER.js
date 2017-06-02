da8499cca7256e6488b48eeea55ebaefe941170f
KristerTarnamaa <krister.tarnamaa@gmail.com>
2017-06-02 16:04:19 +0300

    limitatsioon eemaldatud

--- TYPER.js
+++ TYPER.js
@@ -229,16 +229,10 @@ TYPER.prototype = {
                 this.player.score = this.guessed_words;
 
                 this.timers[this.guessed_words-1] = this.timer;
-                //kui mingi arv sõnu on arvatud, siis mäng on läbi
-                if(this.guessed_words === this.word_amount){
-                    this.guessed_words = 0;
-                    this.player.score -= this.player.errors;
-                    this.stop();
 
-                } else {
+
                     //loosin uue sõna
                     this.generateWord();
-                }
                 this.timer = 0;
             }
             //joonistan uuesti
