efa00d9276100f14a697a625dbcaa99c6cf91dc9
Rauno Tegelmann <raunoot@gmail.com>
2017-06-02 22:22:34 +0300

    Temporary fix, mitigating the issue by adding a seperate page for the typer app itself..

--- TYPER.js
+++ TYPER.js
@@ -213,7 +213,6 @@ function structureArrayByWordLength(words) {
   return temp_array;
 }
 
-
 window.onload = function() {
   var typerGame = new TYPER();
   window.typerGame = typerGame;
@@ -268,26 +267,4 @@ function timer(time, showTime) {
     }
     console.log("timer");
   }, 1000);
-}
-
-//EDETABEL
-function orderScores(p1, p2) {
-    if (p1.score > p2.score) {
-        return -1;
-    }
-    if (p1.score < p2.score) {
-        return 1;
-    }
-    return 0;
-}
-
-function highScores() {
-    var session = JSON.parse(localStorage.getItem("session"));
-    var content = document.getElementsByClassName('topScore')[0];
-
-    session.sort(orderScores);
-
-    for (i = 0; i < session.length; i++) {
-        content.innerHTML += "Score: " + session[i].score + "<br> Name: " + session[i].name + "<br><br>";
-    }
 }
---
+++ scores.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+  highScores();
+};
+
+function orderScores(p1, p2) {
+    if (p1.score > p2.score) {
+        return -1;
+    }
+    if (p1.score < p2.score) {
+        return 1;
+    }
+    return 0;
+}
+
+function highScores() {
+    var session = JSON.parse(localStorage.getItem("session"));
+    var content = document.getElementsByClassName('topScore')[0];
+
+    session.sort(orderScores);
+
+    for (i = 0; i < session.length; i++) {
+        content.innerHTML += "Score: " + session[i].score + "<br> Name: " + session[i].name + "<br><br>";
+    }
+}
