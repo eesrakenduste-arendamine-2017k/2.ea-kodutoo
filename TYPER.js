7da36a2f526747c03da5eb310ffd4c9642acd9b8
Joosep Juhanson <Juhanson@users.noreply.github.com>
2017-06-05 11:40:46 +0300

    nightmode and tidied up typer.js

--- TYPER.js
+++ TYPER.js
@@ -35,6 +35,7 @@ TYPER.prototype = {
 
 	// Funktsioon, mille käivitame alguses
 	init: function(){
+		console.log('Game started.');
 
 		// Lisame canvas elemendi ja contexti
 		this.canvas = document.getElementsByTagName('canvas')[0];
@@ -44,14 +45,16 @@ TYPER.prototype = {
 		this.canvas.style.width = this.WIDTH + 'px';
 		this.canvas.style.height = this.HEIGHT + 'px';
 
-		//resolutsioon 
+		//resolutsioon
 		// kui retina ekraan, siis võib ja peaks olema 2 korda suurem
 		this.canvas.width = this.WIDTH;
 		this.canvas.height = this.HEIGHT;
 
+		this.counter = null;
+
 		// laeme sõnad
 		this.loadWords();
-		
+
 		//loon mängu alustamise tarveks uue muutuja
 		var startgame = document.getElementById("begingame");
 		if(startgame){
@@ -59,8 +62,7 @@ TYPER.prototype = {
 		}
 		window.addEventListener('keypress', this.keyPressed.bind(this));							
 
-	}, 
-
+	},
 	
 	//funktsioon alustamaks mängu ja sätestamaks muutuja väärtused
 	begingame: function (){
@@ -72,26 +74,23 @@ TYPER.prototype = {
 		typerGame.loadPlayerData();
 	},
 	
-	
-	loadPlayerData: function(){
 
+	loadPlayerData: function(){
 		// küsime mängija nime ja muudame objektis nime
 		var p_name = prompt("Sisesta mängija nimi");
-
 		// Kui ei kirjutanud nime või jättis tühjaks
+		
 		if(p_name === null || p_name === ""){
 			p_name = "Tundmatu";
-		
 		}
-
-		// Mänigja objektis muudame nime
+		// Mängija objektis muudame nime
 		this.player.name = p_name; // player =>>> {name:"Romil", score: 0}
-        console.log(this.player);
-	}, 
+		console.log(this.player.name);
+	},
 
 	loadWords: function(){
 
-        console.log('loading...');
+		console.log('loading...');
 
 		// AJAX http://www.w3schools.com/ajax/tryit.asp?filename=tryajax_first
 		var xmlhttp = new XMLHttpRequest();
@@ -104,7 +103,7 @@ TYPER.prototype = {
 			// Sai faili tervenisti kätte
 			if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
 
-                console.log('successfully loaded');
+				console.log('successfully loaded');
 
 				// serveri vastuse sisu
 				var response = xmlhttp.responseText;
@@ -113,37 +112,34 @@ TYPER.prototype = {
 				// tekitame massiivi, faili sisu aluseks, uue sõna algust märgib reavahetuse \n
 				var words_from_file = response.split('\n');
 				//console.log(words_from_file);
-                
-                // Kuna this viitab siin xmlhttp päringule siis tuleb läheneda läbi avaliku muutuja
-                // ehk this.words asemel tuleb kasutada typerGame.words
-                
+
+				// Kuna this viitab siin xmlhttp päringule siis tuleb läheneda läbi avaliku muutuja
+				// ehk this.words asemel tuleb kasutada typerGame.words
+
 				//asendan massiivi
 				typerGame.words = structureArrayByWordLength(words_from_file);
 				console.log(typerGame.words);
-				
-				// küsime mängija andmed
-                typerGame.loadPlayerData();
 
-				// kõik sõnad olemas, alustame mänguga
-				typerGame.start();
 			}
 		};
 
 		xmlhttp.open('GET','./lemmad2013.txt',true);
 		xmlhttp.send();
-	}, 
-
+	},
+	
 	start: function(){
 
 		// Tekitame sõna objekti Word
 		this.generateWord();
 		//console.log(this.word);
 
-        //joonista sõna
+		//joonista sõna
 		this.word.Draw();
+		
+		//nullime skoori
+		typerGame.player.score = 0;
+		
 
-		// Kuulame klahvivajutusi
-		//window.addEventListener('keypress', this.keyPressed.bind(this));
 		this.counter = window.setInterval(function(){
 			typerGame.word.Draw();
 			typerGame.seconds--;
@@ -221,29 +217,26 @@ TYPER.prototype = {
 			}
 		},1000);
 	},
-	
-    generateWord: function(){
 
-        // kui pikk peab sõna tulema, + min pikkus + äraarvatud sõnade arvul jääk 5 jagamisel
-        // iga viie sõna tagant suureneb sõna pikkus ühe võrra
-        var generated_word_length =  this.word_min_length + parseInt(this.guessed_words/5);
+	generateWord: function(){
+		// kui pikk peab sõna tulema, + min pikkus + äraarvatud sõnade arvul jääk 5 jagamisel
+		// iga viie sõna tagant suureneb sõna pikkus ühe võrra
+		var generated_word_length =  this.word_min_length + parseInt(this.guessed_words/5);
 
-    	// Saan suvalise arvu vahemikus 0 - (massiivi pikkus -1)
-    	var random_index = (Math.random()*(this.words[generated_word_length].length-1)).toFixed();
+		// Saan suvalise arvu vahemikus 0 - (massiivi pikkus -1)
+		var random_index = (Math.random()*(this.words[generated_word_length].length-1)).toFixed();
 
-        // random sõna, mille salvestame siia algseks
-    	var word = this.words[generated_word_length][random_index];
-    	
-    	// Word on defineeritud eraldi Word.js failis
-        this.word = new Word(word, this.canvas, this.ctx);
-    },
-    
+		// random sõna, mille salvestame siia algseks
+		var word = this.words[generated_word_length][random_index];
+		//console.log("Word: "+word);
+
+		// Word on defineeritud eraldi Word.js failis
+		this.word = new Word(word, this.canvas, this.ctx);
+	},
+
 	keyPressed: function(event){
-
-		//console.log(event);
 		// event.which annab koodi ja fromcharcode tagastab tähe
 		var letter = String.fromCharCode(event.which);
-		//console.log(letter);
 
 		// Võrdlen kas meie kirjutatud täht on sama mis järele jäänud sõna esimene
 		//console.log(this.word);
@@ -258,47 +251,83 @@ TYPER.prototype = {
 
 				this.guessed_words += 1;
 
-                //update player score
-                this.player.score = this.guessed_words;
-
+				//update player score
+				this.player.score = this.guessed_words;
+				console.log("Score: "+this.player.score);			
+				
 				//loosin uue sõna
 				this.generateWord();
 			}
 
 			//joonistan uuesti
 			this.word.Draw();
-		}
 
-	} // keypress end
-
+		//vigade tracker
+		} else {
+			console.log("Mistype!");
+			this.mistakes = this.mistakes + 1;
+			console.log("This.counter: "+this.mistakes);
+		};
+	},
+	
+	
+	again: function () {
+		this.player.score = 0;
+		this.seconds = 30;
+		console.log("Empty score: "+this.player.score);
+		this.mistakes = 0;
+		typerGame.mistakes = 0;
+		this.guessed_words = 0;
+		this.generateWord();
+		this.word.Draw();
+	},
+	
 };
-
 
 /* HELPERS */
 function structureArrayByWordLength(words){
-    // TEEN massiivi ümber, et oleksid jaotatud pikkuse järgi
-    // NT this.words[3] on kõik kolmetähelised
+	// TEEN massiivi ümber, et oleksid jaotatud pikkuse järgi
+	// NT this.words[3] on kõik kolmetähelised
 
-    // defineerin ajutise massiivi, kus kõik on õiges jrk
-    var temp_array = [];
+	// defineerin ajutise massiivi, kus kõik on õiges jrk
+	var temp_array = [];
 
-    // Käime läbi kõik sõnad
-    for(var i = 0; i < words.length; i++){
+	// Käime läbi kõik sõnad
+	for(var i = 0; i < words.length; i++){
 
-        var word_length = words[i].length;
+		var word_length = words[i].length;
 
-        // Kui pole veel seda array'd olemas, tegu esimese just selle pikkusega sõnaga
-        if(temp_array[word_length] === undefined){
-            // Teen uue
-            temp_array[word_length] = [];
-        }
+		// Kui pole veel seda array'd olemas, tegu esimese just selle pikkusega sõnaga
+		if(temp_array[word_length] === undefined){
+			// Teen uue
+			temp_array[word_length] = [];
+		}
+		// Lisan sõna juurde
+		temp_array[word_length].push(words[i]);
+	}
 
-        // Lisan sõna juurde
-        temp_array[word_length].push(words[i]);
-    }
-
-    return temp_array;
+	return temp_array;
 }
+
+function nightmode(){  //By Aram Oram, from https://codepen.io/Addiosamigo/pen/lArin
+	var color = document.getElementById('color').style.color;
+	var backgroundColor = document.body.style.backgroundColor;
+
+	if (color == "black" && backgroundColor == "white") {
+		document.getElementById('color').style.color="white";
+		document.body.style.backgroundColor="black";
+		typerGame.ctx.fillStyle = "white";
+		document.getElementById("tablePrint").style.borderColor = "white";
+		document.getElementById("scoreboard").style.borderColor = "white";		
+	} else {
+		document.getElementById('color').style.color="black";
+		document.body.style.backgroundColor="white";
+		typerGame.ctx.fillStyle = "black";
+		document.getElementById("tablePrint").style.borderColor = "black";
+		document.getElementById("scoreboard").style.borderColor = "black";
+	}
+};
+
 
 function scoreboard() {
 	
