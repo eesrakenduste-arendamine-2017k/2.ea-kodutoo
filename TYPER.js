2328bf6a498c7e58a8e40e39169e15cbe11f8b4d
Joosep Juhanson <Juhanson@users.noreply.github.com>
2017-06-06 22:25:53 +0300

    some small fixes

--- TYPER.js
+++ TYPER.js
@@ -155,64 +155,64 @@ TYPER.prototype = {
 			
 			if(typerGame.seconds == 0){
 				//aja lõpus lisab mängija nime, tulemuse ja vead kolme massiivi
-				var game = [];
-				var game2 = [];
-				var game3 = [];
+				var savedscore = [];
+				var savedscore2 = [];
+				var savedscore3 = [];
 				
-				var gameinfo = [
+				var savedscorestate = [
 					typerGame.player.name
 				];
 				
-				var gameinfo2 = [
+				var savedscorestate2 = [
 					typerGame.player.score
 				];
 				
-				var gameinfo3 = [
+				var savedscorestate3 = [
 					typerGame.mistakes
 				];
 			
-				console.log(gameinfo);
-				console.log(gameinfo2);
-				console.log(gameinfo3);
+				console.log(savedscorestate);
+				console.log(savedscorestate2);
+				console.log(savedscorestate3);
 				
-				var gamesFromStorage = null;
-				if(localStorage.getItem("game")){
-					gamesFromStorage = JSON.parse(localStorage.getItem("game"));
-					if(gamesFromStorage){
-						game = gamesFromStorage;
+				var savedscoresFromStorage = null;
+				if(localStorage.getItem("savedscore")){
+					savedscoresFromStorage = JSON.parse(localStorage.getItem("savedscore"));
+					if(savedscoresFromStorage){
+						savedscore = savedscoresFromStorage;
 					}
 				}
-				game.push(gameinfo);
-				localStorage.setItem("game", JSON.stringify(game));
+				savedscore.push(savedscorestate);
+				localStorage.setItem("savedscore", JSON.stringify(savedscore));
 				
-				var gamesFromStorage2 = null;
-				if(localStorage.getItem("game2")){
-					gamesFromStorage2 = JSON.parse(localStorage.getItem("game2"));
-					if(gamesFromStorage2){
-						game2 = gamesFromStorage2;
+				var savedscoresFromStorage2 = null;
+				if(localStorage.getItem("savedscore2")){
+					savedscoresFromStorage2 = JSON.parse(localStorage.getItem("savedscore2"));
+					if(savedscoresFromStorage2){
+						savedscore2 = savedscoresFromStorage2;
 					}
 				}
-				game2.push(gameinfo2);
-				localStorage.setItem("game2", JSON.stringify(game2));
+				savedscore2.push(savedscorestate2);
+				localStorage.setItem("savedscore2", JSON.stringify(savedscore2));
 				
-				var gamesFromStorage3 = null;
-				if(localStorage.getItem("game3")){
-					gamesFromStorage3 = JSON.parse(localStorage.getItem("game3"));
-					if(gamesFromStorage3){
-						game3 = gamesFromStorage3;
+				var savedscoresFromStorage3 = null;
+				if(localStorage.getItem("savedscore3")){
+					savedscoresFromStorage3 = JSON.parse(localStorage.getItem("savedscore3"));
+					if(savedscoresFromStorage3){
+						savedscore3 = savedscoresFromStorage3;
 					}
 				}
-				game3.push(gameinfo3);
-				localStorage.setItem("game3", JSON.stringify(game3));
+				savedscore3.push(savedscorestate3);
+				localStorage.setItem("savedscore3", JSON.stringify(savedscore3));
 				
 			//võimalus kohe uuesti mängu alustada	
-			var playagain = confirm(typerGame.player.name + " tulemus:  " + typerGame.player.score +"\nTahad uuesti mängida?");
-				if (playagain){
+			var tryagain = confirm(typerGame.player.name + " tulemus:  " + typerGame.player.score +"\nAlusta uuesti?");
+				if (tryagain){
 					typerGame.guessed_words = 0;
 					typerGame.generateWord();
 					typerGame.player.score = 0;
 					typerGame.loadPlayerData();
-					typerGame.again();
+					typerGame.retry();
 					console.log("Uus tulemus: "+typerGame.player.score);
 				} else {
 					window.clearInterval(typerGame.counter);
@@ -339,31 +339,31 @@ function nightmode(){  //By Aram Oram, from https://codepen.io/Addiosamigo/pen/l
 
 function scoreboard() {
 	
-	var playerdata = localStorage.getItem("game");
-	var playerdata2 = localStorage.getItem("game2");
-	var playerdata3 = localStorage.getItem("game3");
+	var saveddata = localStorage.getItem("savedscore");
+	var saveddata2 = localStorage.getItem("savedscore2");
+	var saveddata3 = localStorage.getItem("savedscore3");
 	
 	var specialChars = '"!@#$^&%*()+=-[]\/{}|:<>?.';
 	for (var i = 0; i < specialChars.length; i++) {
-		playerdata = playerdata.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
-		playerdata2 = playerdata2.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
-		playerdata3 = playerdata3.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+		saveddata = saveddata.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+		saveddata2 = saveddata2.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+		saveddata3 = saveddata3.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
 	}
 	
-	console.log("Array playerdata: "+playerdata);
-	console.log("Array playerdata2: "+playerdata2);
-	console.log("Array playerdata3: "+playerdata3);
+	console.log("Array saved data: "+saveddata);
+	console.log("Array saved data2: "+saveddata2);
+	console.log("Array saved data3: "+saveddata3);
 	
 	
-	playerdata = playerdata.split(',');
-	playerdata2 = playerdata2.split(',');
-	playerdata3 = playerdata3.split(',');
+	saveddata = saveddata.split(',');
+	saveddata2 = saveddata2.split(',');
+	saveddata3 = saveddata3.split(',');
 	
 	var table = "<table>";
 	table = table + "<tr><th>" + "Mängija" +"</th><th>" + "Tulemus" + "</th><th>" + "Vead" + "</th></tr>";
 	
-	for (var i=0; i< playerdata.length; i++) {
-		table = table + "<tr><td>"+ playerdata[i]+"</td><td>"+ playerdata2[i]+"</td><td>"+ playerdata3[i] +"</td></tr>"; 
+	for (var i=0; i< saveddata.length; i++) {
+		table = table + "<tr><td>"+ saveddata[i]+"</td><td>"+ saveddata2[i]+"</td><td>"+ saveddata3[i] +"</td></tr>"; 
 	}
 	table = table + "</table>";
 
